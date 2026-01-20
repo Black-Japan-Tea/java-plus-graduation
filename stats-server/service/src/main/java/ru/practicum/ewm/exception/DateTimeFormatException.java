2d950c0bcff81d52b87a2ec482ebfcdc8b4cb190
Black-Japan-Tea <mister.garry75@yandex.ru>
2026-01-20 21:45:45 +0300

    Реструктуризация модулей

--- stats-server/service/src/main/java/ru/practicum/ewm/exception/DateTimeFormatException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.ewm.exception;
-
-public class DateTimeFormatException extends RuntimeException {
-    public DateTimeFormatException(String message) {
-        super(message);
-    }
-}
